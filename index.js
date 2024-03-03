a44283b20e3da53e25fce93542ecc94c21a9eb67
cyang2026 <cyang2026@gmail.com>
2024-03-03 10:40:12 -0500

    stuff

--- index.js
+++
@@ -1,6 +0,0 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App.js";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-console.log("HI");
