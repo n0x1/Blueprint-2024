869fa876fd181f678566e18d1f3dd20fce0b4992
cyang2026 <cyang2026@gmail.com>
2024-03-03 12:11:48 -0500

    updated list format

--- index.js
+++ index.js
@@ -18,8 +18,42 @@ const toggleHabits = (id) => {
 	if (id === "today") {
 		document.getElementById("this-week").style.display = "none";
 		document.getElementById("today").style.display = "flex";
+		loadHabits("today");
 	} else {
 		document.getElementById("this-week").style.display = "flex";
 		document.getElementById("today").style.display = "none";
+	}
+}
+
+let todayHabits = {
+	"sleep": true,
+	"drink water": false
+};
+let thisWeekHabits = {
+	mon: {},
+	tue: {},
+	wed: {},
+	thu: {},
+	fri: {},
+	sat: {},
+	sun: {},
+}
+
+const loadHabits = (id) => {
+	if (id === "today") {
+		const todaySection = document.getElementById("today");
+		for (const [key, value] of Object.entries(todayHabits)) {
+			console.log(key)
+			const habit = document.createElement("li");
+			habit.innerHTML = key;
+			todaySection.appendChild(habit);
+		}
+
+		for (let i = 0; i < todaySection.children.length; i++) {
+			console.log(i);
+			if (todayHabits[todaySection.children[i].textContent] === false) {
+				todaySection.children[i].style.setProperty("text-decoration", "line-through");
+			}
+		}
 	}
 }
